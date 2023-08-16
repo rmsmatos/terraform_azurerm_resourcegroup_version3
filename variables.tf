30059537a32baa611af9fdd889e3e458c5595c88
Rui Matos <rmsmatos@gmail.com>
2023-08-16 18:22:23 +0100

    Added fixed 4 chars to resourcegroup name composite

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "name" {
   type        = string
   description = "(Required) The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
   validation {
-    condition     = length(var.name) >= 1 && length(var.name) <= 87 && can(regex("^[a-zA-Z0-9-._\\(\\)]+[a-zA-Z0-9-_\\(\\)]$", var.name))
+    condition     = length(var.name) >= 1 && length(var.name) <= 82 && can(regex("^[a-zA-Z0-9-._\\(\\)]+[a-zA-Z0-9-_\\(\\)]$", var.name))
     error_message = "Invalid name (check Azure Resource naming restrictions for more info)."
   }
 }
