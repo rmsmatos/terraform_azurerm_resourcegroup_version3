a2d05e12fccfa6ff6385f2d7aa2db58e8db008f5
Rui Matos <rmsmatos@gmail.com>
2023-08-16 18:37:00 +0100

    Fixed error while composing name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "region" {
 }
 
 resource "azurerm_resource_group" "resourcegroup" {
-  name       = lower(var.environmentShortname != null ? lower("rg-${var.name}-$(module.region.location_fixed4char)-${var.environmentShortname}") : lower("rg-${var.name}-$(module.region.location_fixed4char)"))
+  name       = lower(var.environmentShortname != null ? lower("rg-${var.name}-${module.region.location_fixed4char}-${var.environmentShortname}") : lower("rg-${var.name}-${module.region.location_fixed4char}"))
   location   = module.region.location_cli
   managed_by = var.managed_by
   tags       = var.tags
